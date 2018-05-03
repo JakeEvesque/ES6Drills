168178338c5889ec897a49f55f3f7268ff1ad154
Jake Evesque <Jake@JakesLaptop.EVESQUE.COM>
2018-05-03 12:39:00 -0500

    finished

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+let myFunction = (name, location, favFood) => { console.log(`Name: ${name} Food: ${location} FavFood: ${favFood}`)};
+let myArray = ["Jake", "Babalu's", ["Guacamole"]];
+myFunction(...myArray);
+
+let myName = "Jake Evesque";
+let parseName = (str) => {
+    let myArray = [...str];
+    myArray.forEach((i) => { console.log(i)});    
+};
+console.log("hellow");
+parseName(myName);
+console.log("hellow");
+
+let favMovie = (name = "World", movie = "The Room")   =>  console.log(`Halo, my name is ${name} and my favorite movie is ${movie}`);
+
+let getFirstName = (name) => {return name.split(' ')[0]};
+
+let calculate = (a, b) => ( { product: a * b, squared: Math.pow(a,b)});
+
+
+let calc = calculate(2,4);
+
+console.log(`Product: ${calc.product} Squared: ${calc.squared}`);
 
 
 
-function favMovie(movie) {
-    console.log(`my favorite movie is ${movie}`);
-}
+console.log(`First Name: ${getFirstName("Indigo Montoya")}`);
+favMovie();
+
+favMovie("Indigo Montoya", "The Princess Bride");
+
